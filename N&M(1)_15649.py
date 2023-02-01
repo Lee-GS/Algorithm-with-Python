b1d9339de6674cef6d4a839f68d81563750c8051
Lee-GS <leegs4831@naver.com>
2023-02-01 23:15:12 +0900

    23/02/01  solve

--- N&M(1)_15649.py
+++ N&M(1)_15649.py
@@ -1,4 +1,22 @@
 import sys
-n,m = map(int,sys.stdin.readline.split())
+input = sys.stdin.readline
 
-for i in range ()
+n,m = map(int,input().split())
+ans = []
+visited = [False]*(n+1)
+
+def dfs():
+    if len(ans) == m: # 조건처럼 리스트의 길이가 m이 되면 출력해준다
+        print(' '.join(map(str,ans)))
+        return
+    for i in range(1,n+1):
+        if visited[i]: # 방문했을경우 continue 해준다
+            continue
+        visited[i] = True # 미방문일경우 방문처리를 해준다
+        ans.append(i) # 그리고 리스트에 추가해준다
+        dfs() # 이어서 dfs문을 재귀함수로 실행한다
+        ans.pop() # 마지막에 추가됐던 수를 빼준다.
+        visited[i] = False # 여기가 중요!!!! 방문했던곳을 다시 미방문 처리 해준다
+        
+        
+dfs()
