72594775ce5edfaf03db73cee621200a5922f756
Lee-GS <leegs4831@naver.com>
2023-02-06 21:17:54 +0900

    23/02/06 can't solve

---
+++ 백트래킹/연산자 끼워넣기_14888.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+
+visited = [0]*(len(b))
+
+def dfs():
+    if visited == b:
+        
+
+
+
+
