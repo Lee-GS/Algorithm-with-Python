bc1f9c729062a9ddf123c0a2bb17b4ef0fedb1cd
Lee-GS <leegs4831@naver.com>
2023-02-10 16:15:19 +0900

    23/02/10 solve

--- DP/RGB거리_1149.py
+++ DP/RGB거리_1149.py
@@ -3,28 +3,9 @@ house = []
 for _ in range(n):
     house.append(list(map(int,input().split())))
 
-total = 0
-ans = []
-def dp(array,a,b):
-    global total
-    total += array[a][b]
-    if a == 3:
-        ans.append(total)
-        return ans
-    elif b == 0:
-        if array[a+1][b+1]>=array[a+1][b+2]:
-            dp(array,a+1,b+2)
-        elif array[a+1][b+1]<=array[a+1][b+2]:
-            dp(array,a+1,b+1)
-    elif b == 1:
-        if array[a+1][b-1]>=array[a+1][b+1]:
-            dp(array,a+1,b+1)
-        elif array[a+1][b-1]<=array[a+1][b+1]:
-            dp(array,a+1,b-1)
-    else:
-        if array[a+1][b-2]>=array[a+1][b-1]:
-            dp(array,a+1,b-1)
-        elif array[a+1][b-2]<=array[a+1][b-1]:
-            dp(array,a+1,b-2)
-for i in range(3):
-    dp(house,0,i)
+for i in range(1,n):
+    house[i][0] = min(house[i-1][1],house[i-1][2])+house[i][0]
+    house[i][1] = min(house[i-1][0],house[i-1][2])+house[i][1]
+    house[i][2] = min(house[i-1][0],house[i-1][1])+house[i][2]
+
+print(min(house[n-1]))
