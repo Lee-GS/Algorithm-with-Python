7913f7ac94cf9da531b5352a55c6c2d05b09b848
Lee-GS <leegs4831@naver.com>
2023-02-16 21:02:04 +0900

    23/02/16 can't solve

---
+++ 백트래킹/로또_6603.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+
+
+def dfs():
+    if len(ans) == 6 and ans == sorted(ans):
+        print(' '.join(map(str,ans)))
+        return
+    for i in range(len(lotto)):
+        if visited[i]:
+            continue
+        visited[i] = True
+        ans.append(lotto[i])
+        dfs()
+        ans.pop()
+        visited[i] = False
+
+while True:
+    lotto = list(map(int,input().split()))
+    if lotto[0] == 0:
+        break
+    visited=[False]*(lotto[0])
+    ans = []
+    del lotto[0]
+    dfs()
+    print()
+    
+
+
+    
