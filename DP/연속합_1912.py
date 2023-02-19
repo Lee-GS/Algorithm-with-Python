f83f4b43c219fedd250a853f9c080ae388dbd4b5
Lee-GS <leegs4831@naver.com>
2023-02-19 19:06:59 +0900

    2023/02/19 solve

--- DP/연속합_1912.py
+++ DP/연속합_1912.py
@@ -3,5 +3,7 @@ input = sys.stdin.readline
 
 n =  int(input())
 array = list(map(int,input().split()))
-dp=[0]*n
-ans =[]
+for i in range(1,n):
+    array[i] = max(array[i],array[i]+array[i-1])
+
+print(array)
