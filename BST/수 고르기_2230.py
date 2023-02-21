d486a09cd123e715cf5f7e63365a86ded7bfcea4
Lee-GS <leegs4831@naver.com>
2023-02-21 19:52:36 +0900

    23/02/21 solve

--- BST/수 고르기_2230.py
+++ BST/수 고르기_2230.py
@@ -7,20 +7,19 @@ for _ in range(n):
     a.append(int(input()))
 
 a.sort()
-ans = []
+ans = 10e9
 
-def bs(array,target,start,end):
-    if start>end:
-        return None
-    mid = (start+end)//2
-    if (array[end]-array[start])>=target:
-        ans.append(array[end]-array[start])
-        return bs(array,target,start,mid)
+start = 0
+end = 0
+
+while start<n and end<n: # 리스트의 길이보다 작아야함
+    if (a[end] - a[start]) < m: # m보다 작을경우 end를 늘려 값을 키워준다
+        end+=1
     else:
-        return bs(array,target,mid+1,end)
+        ans=min(ans,a[end]-a[start]) # m보다 크거나 같을경우 start를 늘려 범위를 다시 좁혀준다
+        start+=1
 
-bs(a,m,0,len(a)-1)
-print(min(ans))
-    
+print(ans)
+
 
 
