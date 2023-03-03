d9ca239c4adbacf74810c4d37f7c0ff52e745f4b
Lee-GS <leegs4831@naver.com>
2023-03-04 01:05:15 +0900

    23/03/04 solve

---
+++ 구현/다리놓기_1010.py
@@ -0,0 +1,10 @@
+from itertools import combinations
+import math
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+for _ in range(n):
+    a,b = map(int,input().split())
+    print(math.comb(b,a))
