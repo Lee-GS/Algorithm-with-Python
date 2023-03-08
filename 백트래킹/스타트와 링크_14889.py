dd6f361f8c4193bcef4d33eb17940d7b95919b36
Lee-GS <leegs4831@naver.com>
2023-03-08 18:48:24 +0900

    23/03/08 almost solve

--- 백트래킹/스타트와 링크_14889.py
+++ 백트래킹/스타트와 링크_14889.py
@@ -2,7 +2,38 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-graph = [[0] for _ in range(n+1)]
+graph = []
+start = []
+ans = int(1e9)
 
-for i in range(1,n+1):
-    graph[i].append(map(int,input().split()))
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+
+visited = [False]*n
+
+def dfs(depth,idx):
+    global ans
+    if len(start) == n//2:
+        start_value = 0
+        link_value = 0
+        for i in range(n):
+            for j in range(n):
+                if visited[i] and visited[j]:   
+                    start_value += graph[i][j]
+                elif not visited[i] and not visited[j]:
+                    link_value += graph[i][j]
+    
+        ans=min(ans,abs(start_value-link_value))
+        return
+    
+    for i in range(idx, n):
+        if not visited[i]:
+            visited[i] = True
+            dfs(depth+1, i+1)
+            visited[i] = False
+
+
+dfs(0,0)
+print(ans)
+
+
