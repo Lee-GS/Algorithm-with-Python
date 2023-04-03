1cd6f9d4c9007608fbf228a4d6177aa825a28b22
Lee-GS <leegs4831@naver.com>
2023-04-03 17:42:03 +0900

    23/04/03 solve

--- BST/기타레슨_2343.py
+++ BST/기타레슨_2343.py
@@ -1,3 +1,30 @@
-n,m = int(input().split())
+import sys
+input = sys.stdin.readline
+n,m = map(int,input().split())
 lecture = list(map(int,input().split()))
+total =  sum(lecture)
+result = 10**9
+start = max(lecture)
+end = total
 
+while start<=end:
+    mid = (start+end)//2
+    cnt = 1
+    tmp = 0
+
+    for i in range(n):
+        if(tmp+lecture[i] <= mid):
+            tmp+=lecture[i]
+        else:
+            cnt+=1
+            tmp = lecture[i]
+        if cnt > m:
+            break
+    if cnt > m:
+        start = mid + 1
+    else:
+        end = mid - 1 
+        if (mid >= max(lecture)):
+            result = min(result,mid)
+print(result)
+        
