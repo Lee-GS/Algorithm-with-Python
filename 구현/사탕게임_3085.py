332e35f122c45acd91206e752f1d26bcfff4d9ad
Lee-GS <leegs4831@naver.com>
2023-04-13 23:35:38 +0900

    주석 추가

--- 구현/사탕게임_3085.py
+++ 구현/사탕게임_3085.py
@@ -9,16 +9,18 @@ def check():
     global maxCandy
     for i in range(n):
         cnt = 1
+        #행 탐색
         for j in range(1,n):
-            if candyList[i][j] == candyList[i][j-1]:
+            if candyList[i][j] == candyList[i][j-1]: # 행으로 연속되는 요소가 있을경우
                 cnt+=1
                 maxCandy = max(cnt,maxCandy)
             else:
                 cnt=1
            
         cnt=1
+        #열 탐색
         for j in range(1,n):
-            if candyList[j][i] == candyList[j-1][i]:
+            if candyList[j][i] == candyList[j-1][i]:# 열로 연속되는 경우가 있을경우
                 cnt+=1
                 maxCandy = max(cnt,maxCandy)
             else:
@@ -29,13 +31,19 @@ def check():
 for i in range(n):
     for j in range(n):
         if j+1 < n:
+            #오른쪽꺼와 변겅
             candyList[i][j], candyList[i][j+1] = candyList[i][j+1], candyList[i][j]
+            #검사
             check()
+            # 원위치 시킨다
             candyList[i][j], candyList[i][j+1] = candyList[i][j+1], candyList[i][j] 
         
         if i+1 < n:
+            #아래쪽꺼와 변경
             candyList[i][j], candyList[i+1][j] = candyList[i+1][j], candyList[i][j]
+            #검사
             check()
+            #원위치 시킨다
             candyList[i][j], candyList[i+1][j] = candyList[i+1][j], candyList[i][j] 
 
 print(maxCandy)
