6de2998ffb90823711928622cbe94ed525fa58ae
Lee-GS <leegs4831@naver.com>
2023-04-13 23:35:26 +0900

    23/04/13 solve

--- BFS&DFS/트리의 지름_1967.py
+++ BFS&DFS/트리의 지름_1967.py
@@ -1,41 +1,30 @@
-import heapq
 import sys
+sys.setrecursionlimit(10**9) # 이문제에선 해주지 않았더니 recursionerror가 떴다.
 input = sys.stdin.readline
-INF = int(1e9)
+
 n = int(input())
 graph = [[] for _ in range(n+1)]
 for _ in range(n-1):
     a,b,c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
+    graph[a].append((b,c))# 무방향 그래프이므로
+    graph[b].append((a,c))# 두 노드 모두에 추가시켜준다.
 
-def dijkstra(start):
-    distance=[INF]*(n+1)
-    q=[]
-    heapq.heappush(q,(0,start))
-    distance[start] = 0
+distance = [-1]*(n+1) #거리를 나타낼 리스트이다.
 
-    while q:
-        dist,now = heapq.heappop(q)
-        if distance[now]<dist:
-            continue
-        for i,j in graph[now]:
-            cost = dist+j
-            if distance[i] > cost:
-                distance[i] = cost
-                heapq.heappush(q,(cost,i))
-    del distance[0]
-    return max(distance)
+def dfs(start,cost):
+    for i,j in graph[start]: # 해당 노드의 요소를 뺀다
+        a,b = i,j # 각각 a,b에 매칭시킨다
+        if distance[a] == -1: # 방문하지 않았을경우
+            distance[a] = b + cost # 해당 노드의 거리값과 출발한 노드의 간선의 값을 합해서 넣어준다
+            dfs(a, b+cost) # 재귀를 해준다
 
-_max=0
+distance[1] = 0 # 1번 노드부터 탐색해준다 
+dfs(1,0)
 
-for i in range(1,n+1):
-    tmp = dijkstra(i)
-    if tmp > _max:
-        _max = tmp
-print(_max)
+node = distance.index(max(distance))# 첫번째 탐색이 끝나고 가장 멀리있는 노드를 찾아준다.
+distance = [-1]*(n+1)
+distance[node] = 0 # 가장 멀리있는 노드에서부터 다시 탐색한다
+dfs(node,0)
 
-
-
-
+print(max(distance)) # 최댓값 출력!!
 
